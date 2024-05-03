b754dcd276ff9724bac589dd87065631d04fbea2
HollieFrye <holliekristine@gmail.com>
2024-05-02 20:20:14 -0400

    Testing account
    Testing account

---
+++ Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+
+public class Test{
+    public static void main(String[]args){
+        System.out.println("Hello World");
+        System.out.println("My name is Hollie");
+        int i = 0;
+        int y = 1;
+        System.out.println(i+y);
+    }
+}
