a204f85def12060109e2d0e9862efec88b5972bb
HollieFrye <holliekristine@gmail.com>
2024-05-02 21:25:19 -0400

    Update Test.java

---
+++ Test/Test.java
@@ -0,0 +1,12 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Test{
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("What's your favorite color? ");
+        String favColor = input.nextLine();
+        System.out.println(favColor);
+    }
+}
